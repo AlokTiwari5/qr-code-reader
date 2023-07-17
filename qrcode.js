824e43dbc5fa5501b8971e2e030bccb76bcda3fc
Alok Tiwari <aloktiwari7880@gmail.com>
2023-07-17 20:55:18 +0530

    twentyeighth commit

---
+++ qrcode.js
@@ -0,0 +1,6 @@
+qrcode.result = "error decoding QR Code";
+//...
+qrcode.callback("Failed to load the image");
+qrcode.result = Error("error decoding QR Code");
+//...
+qrcode.callback(Error("Failed to load the image"));
